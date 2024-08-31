1534e140e1fc53896ea75503a799c2a7bc25e837
Jacob Winkler <Jacob.Winkler@oracle.com>
2024-08-31 07:58:25 -0500

    random changes

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const button = document.getElementById('convert-btn');
 const number = document.getElementById('number');
 const output = document.getElementById('output');
 
-button.addEventListener('click', e => {
+button.addEventListener('click', (e) => {
 
     if(number.value === ''){
         let result = document.createElement('p');
         let node = document.createTextNode('Please enter a valid number');
         result.appendChild(node);
-        
-        return output.appendChild(result);
+        output.appendChild(result);
        
     }
-})
+});
 
 
